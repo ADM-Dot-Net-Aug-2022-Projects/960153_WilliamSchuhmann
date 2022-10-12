626886f775e014a82abb9f7dbad1270483d319ea
William Schuhmann <960153@cognizant.com>
2022-10-12 15:05:45 -0700

    Single product on client.

--- EcommerceApp/Client/Services/ProductService/IProductService.cs
+++ EcommerceApp/Client/Services/ProductService/IProductService.cs
@@ -6,5 +6,6 @@ namespace EcommerceApp.Client.Services.ProductService
     {
         List<Product> Products { get; set; }
         Task GetProducts();
+        Task<ServiceResponse<Product>> GetProduct(int productId);
     }
 }
