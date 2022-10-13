509d80199cab8565230fa488c49f6faa0fee5bbb
William Schuhmann <960153@cognizant.com>
2022-10-13 11:19:36 -0700

    Blazored.LocalStorage added successfully

--- EcommerceApp/Client/Program.cs
+++ EcommerceApp/Client/Program.cs
@@ -6,10 +6,13 @@ global using EcommerceApp.Client.Services.CategoryService;
 using EcommerceApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
