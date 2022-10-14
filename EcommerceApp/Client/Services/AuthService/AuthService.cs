ff449a1692ca47e58935f5a45e2f66e57614ba48
William Schuhmann <960153@cognizant.com>
2022-10-13 18:25:24 -0700

    Registration on client

--- EcommerceApp/Client/Services/AuthService/AuthService.cs
+++ EcommerceApp/Client/Services/AuthService/AuthService.cs
@@ -2,5 +2,16 @@
 {
     public class AuthService : IAuthService
     {
+        private readonly HttpClient http;
+
+        public AuthService(HttpClient http)
+        {
+            this.http = http;
+        }
+        public async Task<ServiceResponse<int>> Register(UserRegister request)
+        {
+            var result = await http.PostAsJsonAsync("api/auth/register", request);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+        }
     }
 }
