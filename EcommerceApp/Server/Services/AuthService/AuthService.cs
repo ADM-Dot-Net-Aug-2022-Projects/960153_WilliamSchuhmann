f9037153858bb491b22f0663f20a599936039c4e
William Schuhmann <960153@cognizant.com>
2022-10-13 22:14:38 -0700

    Now able to login

--- EcommerceApp/Server/Services/AuthService/AuthService.cs
+++ EcommerceApp/Server/Services/AuthService/AuthService.cs
@@ -81,7 +81,7 @@ namespace EcommerceApp.Server.Services.AuthService
         }
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using (var hmac = new HMACSHA512())
+            using (var hmac = new HMACSHA512(passwordSalt))
             {
                 var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return computeHash.SequenceEqual(passwordHash);
