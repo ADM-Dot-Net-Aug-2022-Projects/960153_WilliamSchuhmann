3fbc9cee8eb14877a974af3ef91b9d7d48d3584f
William Schuhmann <960153@cognizant.com>
2022-10-13 21:25:03 -0700

    Creeated JSON Web Token

--- EcommerceApp/Server/Services/AuthService/AuthService.cs
+++ EcommerceApp/Server/Services/AuthService/AuthService.cs
@@ -1,22 +1,42 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace EcommerceApp.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         private readonly DataContext context;
-        public AuthService(DataContext context)
+        private readonly IConfiguration configuration;
+
+        public AuthService(DataContext context, IConfiguration configuration)
         {
             this.context = context;
+            this.configuration = configuration;
         }
 
         public async Task<ServiceResponse<string>> Login(string email, string password)
         {
-            var response = new ServiceResponse<string>
+            var response = new ServiceResponse<string>();
+            var user = await context.Users
+                .FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
+            if (user == null)
             {
-                Data = "token"
-            };
+                response.Success = false;
+                response.Message = "User not found.";
+            }
+            else if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong Password.";
+            }
+            else
+            {
+                response.Data = CreateToken(user);
+            }
+            
             return response;
         }
 
@@ -58,6 +78,35 @@ namespace EcommerceApp.Server.Services.AuthService
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
     }
 }
